7fa6cd01dbdcecaf9ff76dab3a2736cf4152f591
Dr. S. Saravanan <saravananms@svce.ac.in>
2026-01-24 12:53:36 +0530

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-function login() {
-    const role = document.getElementById("role").value;
-
-    if (role === "admin") window.location.href = "dashboard.html";
-    if (role === "student") window.location.href = "student.html";
-    if (role === "company") window.location.href = "company.html";
-}
